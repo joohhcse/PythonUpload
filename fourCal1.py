ebcaaf6c6a65b50cebcf680238d7e9fc41c99f63
JOO <joohhcse@gmail.com>
2020-08-26 23:03:53 +0900

    python class
    make python classes

---
+++ fourCal1.py
@@ -0,0 +1,18 @@
+# Four cal 1
+
+class FourCal:
+		def setdata(self, first, second):
+				self.first = first
+				self.second = second
+				
+a = FourCal()
+a.setdata(1,2)
+
+
+
+				
+				
+				
+
+
+
---
+++ fourCal2.py
@@ -0,0 +1,21 @@
+# FourCal 2
+
+class FourCal:
+		def __init__(self, first, second):
+				self.first = first
+				self.second = second
+		def setdata(self, first, second):
+				self.first = first
+				self.second = second
+		def add(self):
+				result = self.first + self.second
+				return result
+				
+a = FourCal(1, 2)
+
+
+		
+				
+				
+				
+				
