d43b06bee6c848799b22779f5012caf9380759b1
JOO <joohhcse@gmail.com>
2020-08-28 22:05:08 +0900

    python sample program

---
+++ sample program/ex_program.py
@@ -0,0 +1,15 @@
+# python ex_program
+
+def GuGu(n):
+		result = []
+		i = 1
+		while i < 10:
+				result.append(n*i)
+				i = i + 1
+		return result
+		
+print(GuGu(2))		#	결과 : [2, 4, 6, 8, 10, 12, 14, 16, 18]
+
+
+
+
