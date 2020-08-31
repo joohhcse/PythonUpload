37c3a43a9ff8337966a35d0f9d1b4e8a7257fbb1
JOO <joohhcse@gmail.com>
2020-08-31 21:10:40 +0900

    python basic

---
+++ Python basic.py
@@ -0,0 +1,22 @@
+# Python basic
+
+# \n : 줄바꿈
+
+# \" \' : 문장 내에서 따옴표
+print("저는 \"나도코딩\" 입니다.")
+print("저는 \'나도코딩\' 입니다.")
+
+# \\ : 문장 내에서 \
+print("C:\\Users\\Nadocoding\\Desktop\\PythonWorkspace")
+
+# \r : 커서를 맨 앞으로 이동
+print("Red Apple\rPine")
+
+# \b : 백스페이스 (한글자 삭제)
+print("Redd\bApple") # RedApple
+
+# \t : 탭
+print("Red\tApple")	# Red	Apple\rPine
+
+
+
