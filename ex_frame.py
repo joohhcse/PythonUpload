8c08004bb30e2c44e6359d82db26e341ba27aeb7
JOO <joohhcse@gmail.com>
2020-09-05 01:00:13 +0900

    ex GUI frame

---
+++ ex_frame.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+root = Tk()
+root.title("GUI")
+root.geometry("640x480") # 가로 세로
+#root.geometry("640x480+300+100") # 가로 세로 + x좌표 + y좌표
+
+root.resizable(False, False) # x너비 y높이 변경 불가
+
+root.mainloop()
+
+
