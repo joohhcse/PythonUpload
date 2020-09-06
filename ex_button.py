e654305baed5522abd675b61728c0de58e288f4c
JOO <joohhcse@gmail.com>
2020-09-06 20:22:13 +0900

    ex GUI btn

---
+++ ex_button.py
@@ -0,0 +1,18 @@
+from tkinter import *
+
+root = Tk()
+root.title("Nado GUI")
+
+btn1 = Button(root, text="버튼1")
+btn1.pack()
+
+btn2 = Button(root, padx=5, pady=10, text="버튼2")
+btn2.pack()
+
+btn3 = Button(root, padx=10, pady=5, text="버튼3")
+btn3.pack()
+
+btn4 = Button(root, width=10, height=3, text="버튼4")
+btn4.pack()
+
+root.mainloop()
